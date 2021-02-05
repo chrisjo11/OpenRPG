6002e23298496e2be3ba8d3961027b6d1771f71b
Christopher Jo <chrisjo1105@gmail.com>
2021-02-05 12:05:17 -0500

    removed useless code

--- movement.py
+++ movement.py
@@ -28,7 +28,6 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
         self.surf = pygame.Surface((75, 25))
-        self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
 
     # Move the sprite based on user keypresses
