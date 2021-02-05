5eb35841eba9434bbad053e38704cb1d813483b1
Christopher Jo <chrisjo1105@gmail.com>
2021-02-05 12:05:28 -0500

    removed useless code

--- ogMovement.py
+++ ogMovement.py
@@ -25,7 +25,7 @@ SCREEN_HEIGHT = 600
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
-        self.surf = pygame.Surface((75, 25))
+        self.surf = pygame.Surface((75, 10))
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
 
