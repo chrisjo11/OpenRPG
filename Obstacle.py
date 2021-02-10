42a3909dd85fb08c7919e65bdff2913a8e652659
Christopher Jo <chrisjo1105@gmail.com>
2021-02-10 11:52:06 -0500

    put all obstacle class code into obstacle file

---
+++ Obstacle.py
@@ -0,0 +1,8 @@
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, obs_spritex, obs_spritey):
+        self.obs_sprite_image = pygame.image.load('player.png')
+        self.image = pygame.transform.scale(self.obs_sprite_image,(50,50))
+        self.obs_sprite_length = 50
+        self.obs_sprite_width = 50
+        self.obs_spritex = obs_spritex
+        self.obs_spritey = obs_spritey
